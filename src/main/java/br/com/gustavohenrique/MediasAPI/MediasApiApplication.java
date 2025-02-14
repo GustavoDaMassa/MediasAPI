50bbce85678d0c85c25abc1ee0947ba4a19ee209
Gustavo <gustavo.pereira@discente.ufg>
2025-02-14 17:00:20 -0300

    inital project structure, layered architecture

---
+++ src/main/java/br/com/gustavohenrique/MediasAPI/MediasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavohenrique.MediasAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavohenrique/MediasAPI/MediasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavohenrique.MediasAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
