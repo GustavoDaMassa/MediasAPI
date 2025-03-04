3e3b974ac4c6f1de1872bee415d7565a687f4354
Gustavo <gustavo.pereira@discente.ufg>
2025-03-04 10:54:34 -0300

    fix: url to pathVariables CourseController

--- src/main/java/br/com/gustavohenrique/MediasAPI/controller/rest/CourseController.java
+++ src/main/java/br/com/gustavohenrique/MediasAPI/controller/rest/CourseController.java
@@ -40,28 +40,28 @@ public class CourseController {
                   .map(course -> modelMapper.map(course,CourseDTO.class)).collect(Collectors.toList()));
     }
 
-    @PatchMapping("{id}/name")
+    @PatchMapping("/{id}/name")
     public ResponseEntity<CourseDTO> updateCourseName(@PathVariable Long userId,@PathVariable Long id,
                                                       @RequestBody @Valid StringRequestDTO nameDto){
             return ResponseEntity.ok(modelMapper.map(courseService.updateCourseName(userId, id, nameDto),
                     CourseDTO.class));
     }
 
-    @PatchMapping("{id}/method")
+    @PatchMapping("/{id}/method")
     public ResponseEntity<CourseDTO> updateCourseMethod(@PathVariable Long userId ,@PathVariable Long id,
                                                         @RequestBody @Valid StringRequestDTO averageMethodDto){
             return ResponseEntity.ok(modelMapper.map(courseService
                     .updateCourseAverageMethod(userId, id, averageMethodDto), CourseDTO.class));
     }
 
-    @PatchMapping("{id}/cutoffgrade")
+    @PatchMapping("/{id}/cutoffgrade")
     public ResponseEntity<CourseDTO> updateCourseCutOffGrade(@PathVariable Long userId ,@PathVariable Long id,
                                                              @RequestBody @Valid DoubleRequestDTO cutOffGradeDto){
             return ResponseEntity.ok(modelMapper.map(courseService.updateCourseCutOffGrade(userId, id, cutOffGradeDto),
                     CourseDTO.class));
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<CourseDTO> deleteCourse(@PathVariable Long userId, @PathVariable Long id){
             return ResponseEntity.ok(modelMapper.map(courseService.deleteCourse(userId, id), CourseDTO.class));
     }
